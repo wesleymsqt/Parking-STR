013e087ff8864972ceb1c7a9904a7550891e287f
Wesley Mesquita <wesleymsqt@alu.ufc.br>
2025-01-28 19:34:27 -0300

    up

--- js/js.js
+++
@@ -1,8 +0,0 @@
-
-/* TOGGLE-BTN */
-const menu = document.getElementById('menu');
-        const menuToggle = document.getElementById('menuToggle');
-
-        menuToggle.addEventListener('click', () => {
-            menu.classList.toggle('open');
-        });
